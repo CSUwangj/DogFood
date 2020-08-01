54aa04fb0b3a61a3bf4e337cec5943d882f94750
CSUwangj <CSUwangj@protonmail.com>
2020-08-02 03:13:48 +0800

    feat: close and open sidebar

---
+++ static/js/sidebar.js
@@ -0,0 +1,17 @@
+function openSidebar() {
+  document.getElementsByName('main')[0].style.marginLeft = '250px'
+  document.getElementsByName('footer')[0].style.marginLeft = '250px'
+  document.getElementsByName('header')[0].style.marginLeft = '250px'
+  document.getElementsByName('sidebar')[0].style.width = '250px'
+  document.getElementsByClassName('sidebar-toggle')[0].onclick = closeSidebar
+  document.getElementsByClassName('sidebar-toggle')[0].childNodes[0].nodeValue = 'X'
+}
+
+function closeSidebar() {
+  document.getElementsByName('main')[0].style.marginLeft = '0'
+  document.getElementsByName('footer')[0].style.marginLeft = '0'
+  document.getElementsByName('header')[0].style.marginLeft = '0'
+  document.getElementsByName('sidebar')[0].style.width = '0'
+  document.getElementsByClassName('sidebar-toggle')[0].onclick = openSidebar
+  document.getElementsByClassName('sidebar-toggle')[0].childNodes[0].nodeValue = 'O'
+}
