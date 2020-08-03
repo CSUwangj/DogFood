81134cff7f03698452189ffa9c6a40253020713c
CSUwangj <CSUwangj@protonmail.com>
2020-08-04 02:08:52 +0800

    fix: footer and header width with sidebar

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -2,6 +2,8 @@ function openSidebar() {
   document.getElementsByName('main')[0].style.marginLeft = '250px'
   document.getElementsByName('footer')[0].style.marginLeft = '250px'
   document.getElementsByName('header')[0].style.marginLeft = '250px'
+  document.getElementsByName('header')[0].style.width = 'calc(100% - 282px)'
+  document.getElementsByName('footer')[0].style.width = 'calc(100% - 282px)'
   document.getElementsByName('sidebar')[0].style.width = '250px'
   document.getElementsByClassName('sidebar-toggle')[0].onclick = closeSidebar
   document.getElementsByClassName('sidebar-toggle')[0].childNodes[0].nodeValue = 'X'
@@ -11,6 +13,8 @@ function closeSidebar() {
   document.getElementsByName('main')[0].style.marginLeft = '0'
   document.getElementsByName('footer')[0].style.marginLeft = '0'
   document.getElementsByName('header')[0].style.marginLeft = '0'
+  document.getElementsByName('header')[0].style.width = 'calc(100% - 32px)'
+  document.getElementsByName('footer')[0].style.width = 'calc(100% - 32px)'
   document.getElementsByName('sidebar')[0].style.width = '0'
   document.getElementsByClassName('sidebar-toggle')[0].onclick = openSidebar
   document.getElementsByClassName('sidebar-toggle')[0].childNodes[0].nodeValue = 'O'
