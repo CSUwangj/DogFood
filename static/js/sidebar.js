fbc3fb2f16941646245f93537ac6bc19c65ee684
CSUwangj <CSUwangj@protonmail.com>
2022-09-11 05:13:24 +0800

    fix: left align sidebar

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,13 +1,23 @@
 const openSidebar = () => {
+  if (document.getElementsByName('sidebar')[0].classList.contains('left')) {
+    document.getElementsByName('header')[0].style.marginLeft = '250px'
+    document.getElementsByName('footer')[0].style.marginLeft = '250px'
+    document.getElementsByName('main')[0].style.marginLeft = '250px'
+  }
   document.getElementsByName('header')[0].style.width = 'calc(100% - 250px)'
   document.getElementsByName('footer')[0].style.width = 'calc(100% - 250px)'
-  document.getElementsByName('main')[0].style.width = 'calc(100% - 250px)'
+  document.getElementsByName('main')[0].style.width = 'calc(100% - 282px)'
   document.getElementsByName('sidebar')[0].style.width = '250px'
   document.getElementsByClassName('sidebar-toggle')[0].onclick = closeSidebar
   document.getElementsByClassName('sidebar-toggle')[0].childNodes[0].nodeValue = 'X'
 }
 
 const closeSidebar = () => {
+  if (document.getElementsByName('sidebar')[0].classList.contains('left')) {
+    document.getElementsByName('header')[0].style.marginLeft = '0'
+    document.getElementsByName('footer')[0].style.marginLeft = '0'
+    document.getElementsByName('main')[0].style.marginLeft = '0'
+  }
   document.getElementsByName('header')[0].style.width = 'calc(100%)'
   document.getElementsByName('footer')[0].style.width = 'calc(100%)'
   document.getElementsByName('main')[0].style.width = 'calc(100% - 32px)'
